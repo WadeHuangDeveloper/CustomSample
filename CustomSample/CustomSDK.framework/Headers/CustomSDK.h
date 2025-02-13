6310546a424379b654ce8a50eacf2f4a6d3c7229
Wade <wadehuangdeveloper@gmail.com>
2025-02-13 11:43:25 +0800

    * Initial project.

---
+++ CustomSample/CustomSDK.framework/Headers/CustomSDK.h
@@ -0,0 +1,18 @@
+//
+//  CustomSDK.h
+//  CustomSDK
+//
+//  Created by Huei-Der Huang on 2025/2/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomSDK.
+FOUNDATION_EXPORT double CustomSDKVersionNumber;
+
+//! Project version string for CustomSDK.
+FOUNDATION_EXPORT const unsigned char CustomSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomSDK/PublicHeader.h>
+
+
